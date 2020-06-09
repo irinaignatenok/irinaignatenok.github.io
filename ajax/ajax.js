c46ec1e297b16487e0b33d1876b68352c931a177
irinaignatenok <specterhauz@gmail.com>
2020-06-09 19:18:56 +0300

    all

---
+++ ajax/ajax.js
@@ -0,0 +1,22 @@
+// 1
+let xhr = new XMLHttpRequest();
+xhr.open('GET' 'https:// file:///Users/irinaignat/Desktop/io/irinaignatenok.github.io/')jax/data:iframe.html)
+
+// 3
+xhr.send();
+
+xhr.onload = function() {
+	if(xhr.status != 200) {
+		console.log(`Error: ${xhr.status}: ${xht.statusText}`)
+	}
+}
+console.log(xhr.reponse);
+
+// xhr.onprogress
+
+
+
+
+
+// xhr.onerror
+
