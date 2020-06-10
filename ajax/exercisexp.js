5f56d6a435bcd62db97cc4bfcb18d4f136cda3f6
irinaignatenok <specterhauz@gmail.com>
2020-06-10 14:47:12 +0300

    exercise

--- ajax/exercisexp.js
+++ ajax/exercisexp.js
@@ -9,7 +9,8 @@ xhr.onload = function() {
 	if(xhr.status != 200) {
 		console.log(`Error: ${xhr.status}: ${xht.statusText}`)
 	} else{
-built(xhr.response)
+ console.log(JSON.parse(xhr.response));
+
 }
 }
 
@@ -25,28 +26,3 @@ xhr.onprogress = function(event) {
 xhr.onerror = function() {
   alert("Request failed");
 };
-const built = (arr) => {
-const root = document.getElementById('root');
-arr.forEach((item, i) => {
-	let div = document.createElement('div');
-	let h1 = document.createElement('h1');
-	let h2 = document.createElement('h2');
-	let img = document.createElement('img');
-
-	h1.innerText = item.name;
-	h2.innerText = item.company.name;
-	img.setAttribute('src', `https://robohash.org/${item.id}?200x200`)
-
-	div.appendChild(h1);
-	div.appendChild(h2);
-	root.appendChild(div);
-
-
-
-})
-}
-
-
-
-
-
