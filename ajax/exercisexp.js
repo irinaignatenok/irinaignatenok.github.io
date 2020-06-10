c2e99517f6a2b88d6fd3a0117f31ad436d6cc564
irinaignatenok <specterhauz@gmail.com>
2020-06-10 15:31:41 +0300

    changes

--- ajax/exercisexp.js
+++ ajax/exercisexp.js
@@ -1,5 +1,5 @@
 let xhr = new XMLHttpRequest();
-xhr.open('GET' 'https://jsonplaceholder.typicode.com/users' true);
+xhr.open('GET' 'https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=dc6zaTOxFJmzC');
 xhr.reponseType = 'json';
 
 // 3
@@ -9,7 +9,7 @@ xhr.onload = function() {
 	if(xhr.status != 200) {
 		console.log(`Error: ${xhr.status}: ${xht.statusText}`)
 	} else{
- console.log(JSON.parse(xhr.response));
+ console.log(xhr.response);
 
 }
 }
