40d53f82d96ea3f247f3d541b94f8a727cfd4b01
irinaignatenok <specterhauz@gmail.com>
2020-06-11 17:09:22 +0300

    all

--- ajax/exercisexp.js
+++ ajax/exercisexp.js
@@ -1,5 +1,6 @@
 let xhr = new XMLHttpRequest();
-xhr.open('GET' 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=');
+let link = 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+xhr.open('GET', link);
 xhr.reponseType = 'json';
 
 // 3
